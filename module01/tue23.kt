b1673d915b8a93b8c498604a74cbe2e2d1c71d4f
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-01-23 11:32:36 -0600

    Created module 1 code Made a hero, and their lv Val and Var

---
+++ module01/tue23.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //println("Hello, world!")
+    //println("My name is Jordan Pham, and I'm learning Kotlin")
+    println("The hero announces their presence to the world")
+    
+    val heroName: String = "Karu"
+    println(heroName)
+    var playerlevel: Int = 4
+    println(playerlevel)
+
+    println("The hero embarks on their journey to become THE ELDEN LORD")
+    playerlevel = 5
+    playerlevel += 1
+    println(playerlevel)
+}
