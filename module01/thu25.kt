ad445b8a034b4662cdc50e5eac5eedad96ae031b
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-01-25 11:10:40 -0600

    Save old version

---
+++ module01/thu25.kt
@@ -0,0 +1,15 @@
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces their presence to the world")
+
+    println(HERO_NAME)
+
+    var playerlevel = 4
+    println(playerlevel)
+
+    println("The hero embarks on their journey to become THE ELDEN LORD")
+    playerlevel += 1
+    println(playerlevel)
+
+}
