5a93c4a2e84c3ca61b55ead5175152b3962e5ab4
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-02-01 09:55:09 -0600

    Made a hello tx

---
+++ week04/println.kt
@@ -0,0 +1,10 @@
+// HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+// TODO
+
+fun main() {
+    println("Hello, World!")
+    println("My name is Jordan Pham")
+    println("I am taking kotlin on ITSE-1329")
+}
