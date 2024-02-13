f8ecf770f78e286c8799bb3b0117bcfea3d81958
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-02-13 11:08:39 -0600

    salary

---
+++ _review/unit02/salary.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var name: String = "Jordan Pham"
+    var age: Int = 17
+    var annualPay: Double = 100000.00
+    println("My name is " + name + ", my age is "+ age+ " and I hope to earn " + annualPay+" per year.")
+}
