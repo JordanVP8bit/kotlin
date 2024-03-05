09db6400779bbb223edce986a335e6e9f61c9490
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-03-05 09:26:49 -0600

    distance

---
+++ week08/fallingDistance.kt
@@ -0,0 +1,13 @@
+fun main() {
+    for (time in 1..10) {
+        val distance = fallingDistance(time.toDouble())
+        println("Time: $time seconds, Distance: $distance meters")
+    }
+}
+
+fun fallingDistance(time: Double): Double {
+    val g = 9.8 // Acceleration due to gravity in m/s^2
+
+    val distance = 0.5 * g * time * time
+    return distance
+}
