3d0c30e9e6975d6ad88f5e661ecb25071c2e7089
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-04-09 12:16:03 -0500

    sdas

--- _review/unit02/unit07/sumNumbers.kt
+++ _review/unit02/unit07/sumNumbers.kt
@@ -1,16 +1,15 @@
 fun main() {
     var sum = 0
-    var number: Int? = null
-    while(number == null) {
-        println("Enter a positive number")
-        number = readln()?.toIntOrNull()
+    var number: Int
+
+    do {
+        println("Enter a positive nonzero integer:")
+        number = readLine()?.toIntOrNull() ?: 0
+    } while (number <= 0)
+
+    for (i in 1..number) {
+        sum += i
     }
-    if (number != null && number > 0) {
-        for (i in 1..number) {
-            sum += i
-        }
-        println("The sum of # from 1 to $number is: $sum")
-    } else {
-        println("Please enter a valid number please")
-    }
+
+    println("The sum of numbers from 1 to $number is: $sum")
 }
