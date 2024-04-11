dd077a6847130bd1325903e05b7dc8e8b517be8b
Jordan Pham <157507428+JordanVP8bit@users.noreply.github.com>
2024-04-11 10:30:44 -0500

    Added an invalid input

--- week11/movierating.kt
+++ week11/movierating.kt
@@ -5,12 +5,18 @@ fun main () {
     println("How old are you?")
     val age = readlnOrNull()!!.toInt()
 
+    if (movieRating == null || age == null) {
+    println("Invalid input")
+    return
+    }
+
     val OldEnough = when {
         movieRating == "PG" -> true
         movieRating == "PG-13" && age >= 13 -> true
         movieRating == "R" && age >= 18 -> true
         else -> false
     }
+
  if (OldEnough) {
      println("Enjoy your movie!")
  } else {
